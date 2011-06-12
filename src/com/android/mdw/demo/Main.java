0e3900d066f6f280671fd1c28f49672ceceacb55
Adrián Catalán <acatalan@elementalgeeks.com>
2011-06-11 23:56:14 -0600

    first commit

---
+++ src/com/android/mdw/demo/Main.java
@@ -0,0 +1,52 @@
+package com.android.mdw.demo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main extends MapActivity implements LocationListener {	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+	
+	@Override
+	public void onLocationChanged(Location location) {}
+
+	@Override
+	public void onProviderDisabled(String provider) {}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {}	
+
+	@Override
+	public void onProviderEnabled(String provider) {}
+		
+}
